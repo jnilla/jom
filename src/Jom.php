309204518578a283419e96b1e58d990a8fa8b1c7
Edric Navarro <digitalcomputer2142@gmail.com>
2022-10-16 16:32:37 -0500

    Class name correction

--- src/Jom.php
+++ src/Jom.php
@@ -10,9 +10,9 @@ use Joomla\CMS\Filesystem\File as JFile;
 use Joomla\CMS\Filesystem\Folder as JFolder;
 
 /**
- * Lara Helper facade
+ * Jom is a Joomla API facade
  */
-class Lara
+class Jom
 {
 	/**
 	 * Dumps the given variables and ends the execution of the script
@@ -270,6 +270,188 @@ class Lara
 	public static function inlineJs($script){
 		self::doc()->addScriptDeclaration($script);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Select database method
+	 *
+	 * @param String $query
+	 * 		Query string
+	 * @param Array $bindings
+	 * 		Query string bindings
+	 *
+	 * @return Array
+	 */
+	public static function select($query, $bindings = []){
+		$db = JFactory::getDBO();
+		
+		// Set Query
+		$query = self::prepareQueryBindings($query, $bindings);
+		$db->setQuery($query);
+
+		// Execute and return
+		return $db->loadAssocList();
+	}
+	
+	/**
+	 * Select database method
+	 *
+	 * @param String $query
+	 * 		Query string
+	 * @param Array $bindings
+	 * 		Query string bindings
+	 *
+	 * @return Array
+	 */
+	public static function selectOne($query, $bindings = []){
+		$db = JFactory::getDBO();
+
+		// Set Query
+		$query = self::prepareQueryBindings($query, $bindings);
+		$db->setQuery($query);
+
+		// Execute and return
+		return $db->loadAssoc();
+	}
+	
+	/**
+	 * Prepare the query bindings
+	 *
+	 * @param String $query 
+	 * 		Query string
+	 * @param Array $bindings 
+	 * 		Query string bindings
+	 *
+	 * @return Array
+	 */
+	public static function prepareQueryBindings($query, $bindings = []){
+		$db = JFactory::getDBO();
+		$time = microtime(true);
+		
+		if(!empty($bindings)){
+			// Note: We can't replace the query params with the preg_replace() 
+			// because this function evaluates escaped characters passed 
+			// in the $replacement argument which causes undesired results.
+			// The function str_replace() does not evaluates escaped 
+			// characters passed in the $replacement argument.
+			
+			// Prepare placeholders
+			foreach($bindings as $bindingKey => $bindingValue){
+				$placeholder = "@@-placeholder-$bindingKey-$time-@@";
+				$bindingKey = preg_quote($bindingKey, '/');
+				$query = preg_replace('/\:'.$bindingKey.'\b/', $placeholder, $query);
+			}
+
+			// Replace placeholders
+			foreach($bindings as $bindingKey => $bindingValue){
+				$placeholder = "@@-placeholder-$bindingKey-$time-@@";
+				$bindingValue = $db->escape($bindingValue);
+				$query= str_replace($placeholder, $bindingValue, $query);
+			}
+		}
+
+		return "$query;";
+	}
+	
+	/**
+	 * Update database method
+	 *
+	 * @param String $query
+	 * 		Query string
+	 * @param Array $bindings
+	 * 		Query string bindings
+	 *
+	 * @return Integer
+	 * 		Number of effected rows
+	 */
+	public static function update($query, $bindings = []){
+		$db = JFactory::getDBO();
+		
+		// Set Query
+		$query = self::prepareQueryBindings($query, $bindings);
+		$db->setQuery($query);
+
+		// Execute
+		$db->execute();
+		
+		// Return
+		return $db->getAffectedRows();
+	}
+	
+	/**
+	 * Determine if any rows exist
+	 *
+	 * @param String $query
+	 * 		Query string
+	 * @param Array $bindings
+	 * 		Query string bindings
+	 *
+	 * @return Array
+	 */
+	public static function exists($query, $bindings = []){
+		$db = JFactory::getDBO();
+		
+		// Set Query
+		$query = self::prepareQueryBindings($query, $bindings);
+		$db->setQuery($query);
+		
+		// Execute and return
+		return $db->loadAssocList();
+	}
+
+	/**
+	 * Insert database method
+	 *
+	 * @param String $query
+	 * 		Query string
+	 * @param Array $bindings
+	 * 		Query string bindings
+	 *
+	 * @return Boolean
+	 * 		True on success
+	 */
+	public static function insert($query, $bindings = []){
+		$db = JFactory::getDBO();
+		
+		// Set Query
+		$query = self::prepareQueryBindings($query, $bindings);
+		$db->setQuery($query);
+
+		// Execute and return
+		return ($db->execute() !== false) ? true : false;
+	}
+
+	/**
+	 * Delete database method
+	 *
+	 * @param String $query
+	 * 		Query string
+	 * @param Array $bindings
+	 * 		Query string bindings
+	 *
+	 * @return Integer
+	 * 		Number of effected rows
+	 */
+	public static function delete($query, $bindings = []){
+		$db = JFactory::getDBO();
+		
+		// Set Query
+		$query = self::prepareQueryBindings($query, $bindings);
+		$db->setQuery($query);
+
+		// Execute
+		$db->execute();
+
+		// Return
+		return $db->getAffectedRows();
+	}
+	
 }
 
 
